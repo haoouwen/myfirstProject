6241e090c24fb22a8860547d6b1246393294ff12
haokexin123 <275865210@qq.com>
2017-07-04 22:00:45 +0800

    呵呵

---
+++ src/myfirstProject/HelloWorld.java
@@ -0,0 +1,12 @@
+package myfirstProject;
+
+public class HelloWorld {
+
+	 public static void main(String[] args) {
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+		System.out.println("helloworld");
+	}
+	
+}
