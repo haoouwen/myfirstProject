1832871609e7b58e0a2008e867b7aecb065733c0
Administrator <Administrator@USER-20170608NM>
2017-07-05 00:05:10 +0800

    我的代码谁都不能改

--- src/myfirstProject/HelloWorld.java
+++ src/myfirstProject/HelloWorld.java
@@ -3,10 +3,10 @@ package myfirstProject;
 public class HelloWorld {
 
 	 public static void main(String[] args) {
-		System.out.println("helloworld");
-		System.out.println("helloworld");
-		System.out.println("helloworld");
-		System.out.println("helloworld");
+		System.out.println("就闹");
+		System.out.println("就闹");
+		System.out.println("就闹");
+		System.out.println("就闹");
 	}
 	
 }
